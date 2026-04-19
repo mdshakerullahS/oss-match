d4eeb05afa659d13daa1eb41656778d4ba7a89e3
Shakerullah <sourovmdshakerullah@gmail.com>
2026-04-19 18:07:36 +0600

    update: update packages, update prisma config for docker and add comment in gitignore for prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,36 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/generated/prisma/client";
 
+const databaseURL = process.env.DATABASE_URL;
+
+if (
+  !databaseURL &&
+  (!process.env.POSTGRES_USER ||
+    !process.env.POSTGRES_PASSWORD ||
+    !process.env.POSTGRES_HOST ||
+    !process.env.POSTGRES_PORT ||
+    !process.env.POSTGRES_DB)
+) {
+  throw new Error("Database configuration is missing");
+}
+
+const adapter = new PrismaPg({
+  connectionString:
+    databaseURL ??
+    `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+});
+
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
-const connectionString = process.env.DATABASE_URL!;
-
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    adapter: new PrismaPg({ connectionString }),
+    adapter,
+    log: ["error", "warn"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
